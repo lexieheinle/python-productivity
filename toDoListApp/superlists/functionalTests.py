8dfebbdf445339e84d88933821a8d985fec6f412
lexieheinle <lexie.heinle@gmail.com>
2015-11-07 11:24:01 -0600

    List takes one list item

--- toDoListApp/superlists/functionalTests.py
+++ toDoListApp/superlists/functionalTests.py
@@ -25,28 +25,29 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        import time
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-          any(row.text == '1: Buy peacock feathers' for row in rows),
-          "New to-do item did not appear in table -- its text was:\n%s" % (
-            table.text,
-            )
-        )
+        #import time
+        #time.sleep(10)
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings="ignore")
 
-# The page updates again, and now shows both items on her list
 
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
+
+
 
 # She visits that URL - her to-do list is still there.
 
--- toDoListApp/superlists/lists/tests.py
+++ toDoListApp/superlists/lists/tests.py
@@ -24,6 +24,6 @@ class HomePageTest(TestCase):
     self.assertIn('A new list item', response.content.decode())
     expected_html = render_to_string(
       'index.html',
-      {'new_item_text': 'A new list item'},
+      {'new_item_text': 'A new list item'}
     )
     self.assertEqual(response.content.decode(), expected_html)
