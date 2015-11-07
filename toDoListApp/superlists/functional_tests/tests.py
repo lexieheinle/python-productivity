d4e2ca5194af8816a25cc64316e9e7ed25d9ff44
lexieheinle <lexie.heinle@gmail.com>
2015-11-07 12:38:52 -0600

    make functional_tests an app, use LiveServerTestCase

--- toDoListApp/superlists/functional_tests/tests.py
+++ toDoListApp/superlists/functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
 # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         # She notices the page title and header mention to-do lists
         self.assertIn ('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -46,9 +46,7 @@ class NewVisitorTest(unittest.TestCase):
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
         self.fail('Finish the test!')
-
-if __name__ == '__main__':
-    unittest.main(warnings="ignore")
+        self.browser.quit()
 
 
 
@@ -58,4 +56,3 @@ if __name__ == '__main__':
 
 # Satisfied, she goes back to sleep
 
-browser.quit()
