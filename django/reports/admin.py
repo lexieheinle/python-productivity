78b61f06c49dd0f13e721f086351463e608853c8
lexieheinle <lexie.heinle@gmail.com>
2015-11-11 19:53:56 -0600

    Remove django apps from repo

--- django/reports/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import County, Lake, Report
-
-admin.site.register(County)
-admin.site.register(Lake)
-admin.site.register(Report)
--- toDoListApp/superlists/lists/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class List(models.Model):
-    pass
-
-
-class Item(models.Model):
-    text = models.TextField(default='')
-    list = models.ForeignKey(List, default=None)
