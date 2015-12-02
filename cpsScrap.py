42a655e1134d3b150998baa535f13741e277005c
lexieheinle <lexie.heinle@gmail.com>
2015-12-02 15:06:45 -0600

    Working on making Unemployment in django

--- cpsScrap.py
+++ cpsScrap.py
@@ -2,6 +2,7 @@
 #uses python3
 import urllib.request
 from bs4 import BeautifulSoup
+from reports.models import Unemployment
 
 url = "http://www.bls.gov/cps/cpsaat01.htm" #access the search term through website
 page = urllib.request.urlopen(url).read()
@@ -25,3 +26,21 @@ for row in rows[1:]:
   data.append(dataRow)
 data.pop()
 print(data)
+cleanedRate = []
+cleanedDate = []
+for da in data:
+  #da[0] = eval(da[0])
+  try:
+    if int(da[0]) >= 1947:
+      dates = "{}-{}-{}".format(da[0], 1, 1)
+      cleanedDate.append(dates)
+      cleanedRate.append(eval(da[-2]))
+  except ValueError:
+    pass
+#cleaned.pop(0)
+print(cleanedRate)
+print(cleanedDate)
+length = len(cleanedRate)
+for i in range(length):
+  b, bcreated = Unemployment.objects.get_or_create(rate=cleanedRate[i], date=cleanedDate[i])
+  print(b)
