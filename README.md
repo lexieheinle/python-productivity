14e0b25ef1d29b27fdc14d59bcf0b918fbf34d32
lexieheinle <lexie.heinle@gmail.com>
2016-02-18 17:50:41 -0600

    Add code issues badge

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-productivity
-Here is my collection of productivity programs in python. From a file manager to webscrappers, these programs were created to make my life easier. 
+[![Code Issues](https://www.quantifiedcode.com/api/v1/project/2ebccb9c44ee4642a20b927dba88df7f/badge.svg)](https://www.quantifiedcode.com/app/project/2ebccb9c44ee4642a20b927dba88df7f)
+
+Here is my collection of productivity programs in python. From a file manager to web scrappers, these programs were created to make my life easier. 
--- nnnScrap.py
+++ nnnScrap.py
@@ -3,37 +3,38 @@ from bs4 import BeautifulSoup
 import pdfkit
 import urllib.request
 
-def find-articles(website, firstName, lastName):
-    url = "{0}?s={1}+{2}".format(website, firstName, lastName) #access the search term through website
+def find-articles(website, first-name, last-name):
+    'find all Nebrask news service articles for first-name and last-name reporter and outputs html and pdfs'
+    url = "{0}?s={1}+{2}".format(website, first-name, last-name) #access the search term through website
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html,from_encoding="utf-8")
     articles = soup.findAll('article') #find all articles
-    articleLink = []
+    article-link = []
     for article in articles:
         titles = article.find_all('h2')
         for title in titles:
             links = title.find_all('a')
             for link in links:
-                articleLink.append(link.get('href')) #push article links to array
-    for article in articleLink:
+                article-link.append(link.get('href')) #push article links to array
+    for article in article-link:
         html = urllib.request.urlopen(article).read()
         soup = BeautifulSoup(html)
-        articleTitle = soup.find('h1').prettify(formatter='html')
-        print (articleTitle)
-        articleAuthor = soup.find('p', class_="post-byline").prettify(formatter='html')
-        print (articleAuthor)
-        articleContent = soup.find('div', class_="entry-inner").prettify(formatter='html')
-        print (str(articleContent))
-        for content in str(articleContent):
+        article-title = soup.find('h1').prettify(formatter='html')
+        print (article-title)
+        article-author = soup.find('p', class_="post-byline").prettify(formatter='html')
+        print (article-author)
+        article-content = soup.find('div', class_="entry-inner").prettify(formatter='html')
+        print (str(article-content))
+        for content in str(article-content):
           content = content.replace('’', '\'').replace('“', '"').replace('”', '"')
-        print(str(articleContent))
-        splitArticle = str(article).split('/')
-        shortTitle = splitArticle[-2]
-        print (shortTitle) #same as headline
-        f = open('{0}.html'.format(shortTitle), 'w') #create html file
-        message = str(articleTitle) + str(articleAuthor) + str(articleContent) + str('<a href={0} target="_blank">View the article on the original website</a>'.format(article))
+        print(str(article-content))
+        split-article = str(article).split('/')
+        short-title = split-article[-2]
+        print (short-title) #same as headline
+        f = open('{0}.html'.format(short-title), 'w') #create html file
+        message = str(article-title) + str(article-author) + str(article-content) + str('<a href={0} target="_blank">View the article on the original website</a>'.format(article))
         f.write(message)
         f.close()
-        pdfkit.from_file('{0}.html'.format(shortTitle),'{0}.pdf'.format(shortTitle)) #create pdf version
+        pdfkit.from_file('{0}.html'.format(short-title),'{0}.pdf'.format(short-title)) #create pdf version
 
 articles = find-articles("http://www.newsnetnebraska.org/", "lexie", "heinle")
